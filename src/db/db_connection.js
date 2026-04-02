a66cfbbec9b1754abb9800933d542ab2356e3613
fraylontechnologies-boop <fraylontechnologies@gmail.com>
2026-04-02 18:58:55 +0530

    Fix: Switched to better-sqlite3 for cloud compatibility

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const path = require('path');
 
 const db = knex({
-  client: 'sqlite3',
+  client: 'better-sqlite3',
   connection: {
     filename: path.join(__dirname, '../../database.sqlite'),
   },
